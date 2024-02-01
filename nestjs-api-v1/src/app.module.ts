fdfadb125cf2145ba754c62cfd661c253c602f1d
Joctan Silva <joctan.silva@trinusco.com.br>
2024-02-01 18:41:37 -0300

    bora!

---
+++ nestjs-api-v1/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './products/entities/product.entity';
+import { OrdersModule } from './orders/orders.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: 'root',
+      database: 'nest',
+      entities: [Product],
+      synchronize: true,
+      logging: true,
+    }),
+    OrdersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
