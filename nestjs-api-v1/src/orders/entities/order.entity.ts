62d6a13f705cb3222bc099dec9666de321c8e09d
Joctan Silva <joctan.silva@trinusco.com.br>
2024-02-02 08:45:09 -0300

    feat: adjust order module, and database

--- nestjs-api-v1/src/orders/entities/order.entity.ts
+++ nestjs-api-v1/src/orders/entities/order.entity.ts
@@ -1,1 +1,30 @@
-export class Order {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+export enum OderStatus {
+  PENDING = 'pending',
+  PAID = 'paid',
+  FAILED = 'failed',
+}
+
+@Entity()
+export class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  total: number;
+
+  @Column()
+  client_id: number; //authenticated user id
+
+  @Column()
+  status: OderStatus = OderStatus.PENDING;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
